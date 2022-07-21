3021d0dce62d4272e6bfd4a55dd2fe286b4e168d
Zeeshan Nasir <109717545+zeeshan-attentive@users.noreply.github.com>
2022-07-21 12:49:32 +0530

    Changed to google imagery satellite

--- src/components/Map/Mapped.jsx
+++ src/components/Map/Mapped.jsx
@@ -2,23 +2,26 @@ import React, { useRef } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
-// import XYZ from "ol/source/XYZ";
-import OSM from "ol/source/OSM";
+import XYZ from "ol/source/XYZ";
+// import OSM from "ol/source/OSM";
 import "./mapped.css";
 import { useEffect } from "react";
 
 const Mapped = () => {
     const mapElement = useRef();
 
+    const GOOGLE_IMAGERY_SATELLITE =
+        "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}?v=1";
+
     useEffect(() => {
         new Map({
             target: mapElement.current,
             layers: [
                 new TileLayer({
-                    // source: new XYZ({
-                    // url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    // }),
-                    source: new OSM(),
+                    source: new XYZ({
+                        url: GOOGLE_IMAGERY_SATELLITE,
+                    }),
+                    // source: new OSM(),
                 }),
             ],
             view: new View({
