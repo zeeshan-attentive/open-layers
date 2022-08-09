ce928f1f5847fd94264f798774183149e5aad944
Zeeshan Nasir <109717545+zeeshan-attentive@users.noreply.github.com>
2022-08-09 11:13:33 +0530

    made the toolbar code more generic

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,30 +4,23 @@ import DrawTool from "../Tools/DrawTool";
 import { MapContext } from "./MapComponent";
 
 const Toolbar = () => {
-  const [cancelBox, setCancelBox] = useState("none");
   const [cancelBox1, setCancelBox1] = useState("none");
   const [cancelBox2, setCancelBox2] = useState("none");
+  const [cancelBox3, setCancelBox3] = useState("none");
   const [popupFlag, setPopupFlag] = useState(false);
 
   const map = useContext(MapContext);
 
-  const handleCancel = (type) => {
+  const handleCancel = (type, box) => {
+    if (box === 1) {
+      setCancelBox1("none");
+    } else if (box === 2) {
+      setCancelBox2("none");
+    } else {
+      setCancelBox3("none");
+    }
     map.cancelInteraction();
     map.cancelEdit(type);
-    setCancelBox("none");
-    setPopupFlag(false);
-  };
-
-  const handleCancel1 = () => {
-    map.cancelInteraction();
-    map.cancelEdit();
-    setCancelBox1("none");
-    setPopupFlag(false);
-  };
-
-  const handleCancel2 = () => {
-    map.cancelInteraction();
-    setCancelBox2("none");
     setPopupFlag(false);
   };
 
@@ -38,14 +31,14 @@ const Toolbar = () => {
   return (
     <div className="tools-div">
       <DrawTool
-        setCancelBox={setCancelBox}
+        setCancelBox={setCancelBox1}
         geomType={GEOMETRY_TYPE.LINESTRING}
         setPopupFlag={setPopupFlag}
         popupFlag={popupFlag}
         image={"https://cdn-icons-png.flaticon.com/512/876/876225.png"}
       />
       <DrawTool
-        setCancelBox={setCancelBox1}
+        setCancelBox={setCancelBox2}
         geomType={GEOMETRY_TYPE.POLYGON}
         setPopupFlag={setPopupFlag}
         popupFlag={popupFlag}
@@ -54,7 +47,7 @@ const Toolbar = () => {
         }
       />
       <DrawTool
-        setCancelBox={setCancelBox2}
+        setCancelBox={setCancelBox3}
         geomType={GEOMETRY_TYPE.POINT}
         setPopupFlag={setPopupFlag}
         popupFlag={popupFlag}
@@ -62,20 +55,32 @@ const Toolbar = () => {
           "https://icons-for-free.com/iconfiles/png/512/map+point+icon-1320073183505881976.png"
         }
       />
-      <div className="cancel-box" style={{ display: cancelBox }}>
+      <div className="cancel-box" style={{ display: cancelBox1 }}>
         <span
-          onClick={() => handleCancel(GEOMETRY_TYPE.LINESTRING)}
+          onClick={() => handleCancel(GEOMETRY_TYPE.LINESTRING, 1)}
           style={{ marginRight: "10px" }}
         >
           Cancel
         </span>
         <span onClick={() => handleEdit(GEOMETRY_TYPE.LINESTRING)}>Edit</span>
       </div>
-      <div className="cancel-box1" style={{ display: cancelBox1 }}>
-        <span onClick={handleCancel1}>Cancel</span>
+      <div className="cancel-box1" style={{ display: cancelBox2 }}>
+        <span
+          onClick={() => handleCancel(GEOMETRY_TYPE.POLYGON, 2)}
+          style={{ marginRight: "10px" }}
+        >
+          Cancel
+        </span>
+        <span onClick={() => handleEdit(GEOMETRY_TYPE.POLYGON, 3)}>Edit</span>
       </div>
-      <div className="cancel-box2" style={{ display: cancelBox2 }}>
-        <span onClick={handleCancel2}>Cancel</span>
+      <div className="cancel-box2" style={{ display: cancelBox3 }}>
+        <span
+          onClick={() => handleCancel(GEOMETRY_TYPE.POINT)}
+          style={{ marginRight: "10px" }}
+        >
+          Cancel
+        </span>
+        <span onClick={() => handleEdit(GEOMETRY_TYPE.POINT)}>Edit</span>
       </div>
     </div>
   );
